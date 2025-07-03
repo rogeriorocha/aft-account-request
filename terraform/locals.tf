a8109ab7d7c311b726af73f76962bc596dcb4251
Omar Din <omardin@Omars-MacBook-Air.local>
2025-07-03 06:19:16 +0100

    adding security OU

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,6 +2,40 @@ locals {
   email_secrets  = jsondecode(data.aws_secretsmanager_secret_version.aft_account_secrets_versions.secret_string)
   sso_user_email = local.email_secrets["sso_user_email"]
   accounts = {
+    ##################################################################
+    # SECURITY OU
+    ##################################################################
+    security = {
+      name              = "Security Account"
+      organization_unit = "Security"
+      email             = local.email_secrets["security_account_email"]
+      sso_email         = local.sso_user_email
+      first_name        = "Security"
+      last_name         = "User"
+      tags = {
+        "ABC:Environment" = "SECURITY"
+      }
+      customizations_name = "SECURITY"
+    }
+    logging = {
+      name              = "Logging Account"
+      organization_unit = "Security"
+      email             = local.email_secrets["logging_account_email"]
+      sso_email         = local.sso_user_email
+      first_name        = "Logging"
+      last_name         = "User"
+      tags = {
+        "ABC:Environment" = "LOGGING"
+      }
+      customizations_name = "LOGGING"
+    }
+
+
+
+    ##################################################################
+    # OPS OU
+    ##################################################################
+
     ops = {
       name              = "Ops Account"
       organization_unit = "Ops"
@@ -14,6 +48,11 @@ locals {
       }
       customizations_name = "OPS"
     }
+
+    ##################################################################
+    # PRODUCT OU
+    ##################################################################
+
     production = {
       name              = "Production Account"
       organization_unit = "Product"

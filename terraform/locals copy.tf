708cc48e57b3870fea859d76664d23edb8d67603
Rogerio Rocha <rogerio.rocha@zup.com.br>
2025-10-02 12:11:39 -0300

    [Rogerio Rocha] initial

--- terraform/locals copy.tf
+++ terraform/locals copy.tf
@@ -3,6 +3,38 @@ locals {
   sso_user_email = local.email_secrets["sso_user_email"]
   accounts = {
     ##################################################################
+    # SECURITY OU
+    ##################################################################
+    security = {
+      name              = "Security Account"
+      organization_unit = "Security"
+      email             = local.email_secrets["security_account_email"]
+      sso_email         = local.sso_user_email
+      first_name        = "Security"
+      last_name         = "User"
+      tags = {
+        "ABC:Environment" = "SECURITY"
+      }
+      customizations_name = "SECURITY"
+    }
+
+
+    logging = {
+      name              = "Logging Account"
+      organization_unit = "Security"
+      email             = local.email_secrets["logging_account_email"]
+      sso_email         = local.sso_user_email
+      first_name        = "Logging"
+      last_name         = "User"
+      tags = {
+        "ABC:Environment" = "LOGGING"
+      }
+      customizations_name = "LOGGING"
+    }
+
+
+
+    ##################################################################
     # OPS OU
     ##################################################################
 
@@ -23,6 +55,26 @@ locals {
     # PRODUCT OU
     ##################################################################
 
+    production = {
+      name              = "Production Account"
+      organization_unit = "Product"
+      email             = local.email_secrets["production_account_email"]
+      sso_email         = local.sso_user_email
+      first_name        = "Production"
+      last_name         = "User"
+      tags = {
+        "ABC:Environment" = "PROD"
+      }
+      customizations_name = "PRODUCTION"
+      alternate_contact = {
+        operations = {
+          "email-address" = local.email_secrets["head_of_product_ou_email"]
+          "name"          = "Head of Product"
+          "phone-number"  = "+11234567890"
+          "title"         = "Head of Product"
+        }
+      }
+    }
     development = {
       name              = "Development Account"
       organization_unit = "Product"
